95320cd1c28a634ac9a280adab66e9c4ba7c20de
edz <edz@edzdeMacBook-Air-2.local>
2019-10-27 21:04:20 +0800

    modify

--- ch04/practice/p25.c
+++ ch04/practice/p25.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-int main(int argc, char const *argv[]) {
-    int n;
-    printf("%s\n", "让我们来画一个向下的金字塔。");
-    printf("%s", "金字塔有几层：");
+int main() {
+    int i, j, n, num;
+    puts("让我们来画一个金字塔。") ;
+    printf("金字塔有几层：");
     scanf("%d", &n);
-
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j < i; ++j) {
-            printf(" ");
+    for (i = 1; i <= n; i++) {
+        num = 1;
+        for (j = 1; j <= 2 * n - 1 ; j++) {
+            if (j <= (n - i)) {
+                printf(" ") ;
+            } else if (num <= 2 * i - 1) {
+                printf("*");
+                num++;
+            }
         }
 
-        for (int j = 0; j < 2 * (n - i + 1) - 1; ++j) {
-            printf("%d", i);
-        }
-
-        /* code */
-        printf("\n");
+        printf("\n") ;
     }
 
-    return 0;
+    return 0 ;
 }
